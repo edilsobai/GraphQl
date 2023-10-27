93e7d40d5382adc5513faac03a319dffcceda046
Edil Usobaiev <usobaiev1@gmail.com>
2023-10-27 19:27:13 +0600

    Creating a GraphQ: schema

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express")
+const { buildSchema } = require("graphql")
+
+const schema = buildSchema(`
+  type Query {
+    description: String
+    price: Float
+  }
+`)
 
 const app = express()
+
+
 
 app.listen(5000, ()=> console.log("Server running on port 5000"))
