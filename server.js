260aebae8abfb898827af0c8b49495d5643b03ac
Edil Usobaiev <usobaiev1@gmail.com>
2023-10-30 10:56:47 +0600

    Adding the resolvers

--- server.js
+++ server.js
@@ -8,7 +8,19 @@ const { loadFilesSync } = require("@graphql-tools/load-files")
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"))
 
 const schema = makeExecutableSchema({
-  typeDefs: typesArray
+  typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: (parent, args, context, info) => {
+        console.log("Getting the products...")
+        return parent.products
+      },
+      orders: (parent) => {
+        console.log("Getting the orders...")
+        return parent.orders
+      }
+    }
+  }
 })
 
 const root = {
