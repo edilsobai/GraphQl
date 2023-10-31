37f4196192ca482f30099c591a60fcf0b0aa8a4a
Edil Usobaiev <usobaiev1@gmail.com>
2023-10-31 11:21:57 +0600

    Adding mutation(newProduct)

--- products/products.model.js
+++ products/products.model.js
@@ -2,12 +2,14 @@ const products = [
   {
     id: "redshoe",
     description: "Red Shoes",
-    price: 42.12
+    price: 42.12,
+    reviews: []
   },
   {
     id: "bluejean",
     description: "Blue Jeans",
-    price: 55.55
+    price: 55.55,
+    reviews: []
   }
 ]
 
@@ -22,8 +24,20 @@ function getProductsByPrice(min, max) {
 function getProductById(id) {
   return products.find((product)=> product.id === id)
 }
+
+function addNewProduct(id, description, price){
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: []
+  }
+  
+  return newProduct
+}
 module.exports = {
   getAllProducts,
   getProductsByPrice,
-  getProductById
+  getProductById,
+  addNewProduct
 }
